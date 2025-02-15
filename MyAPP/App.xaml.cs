3b809fc266825f1d51b2cd2dd2502d705de5df48
Marvin Antonio Mejia Espinoza <srgmejia@icloud.com>
2025-02-15 16:16:52 -0600

    Agregar archivos de proyecto.

---
+++ MyAPP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MyAPP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyAPP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
